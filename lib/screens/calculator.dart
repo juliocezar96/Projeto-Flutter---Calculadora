82dbc4e5a52537491222b828d4f90f915cb37922
juliocezar96 <juliocr.jc38@gmail.com>
2021-03-15 20:10:02 -0300

    [calculator] - Implementation interaction

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import '../components/display.dart';
+import '../components/keyboard.dart';
 
 class Calculator extends StatelessWidget {
+  _onPressed(String text) {
+    print(text);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Column(
       children: <Widget>[
-        Display('123'),
-        Text('Keyboard'),
+        Display('123.45'),
+        Keyboard(_onPressed),
       ],
     ));
   }
