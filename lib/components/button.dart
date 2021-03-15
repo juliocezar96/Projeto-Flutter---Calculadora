b9e3e880c9cf512b40c2748e7d271b4ce33c4633
juliocezar96 <juliocr.jc38@gmail.com>
2021-03-15 19:57:23 -0300

    [calculator] - Implementation style button

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -2,12 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
+  static const DARK = Color.fromRGBO(82, 82, 82, 1);
+  static const DEFAULT = Color.fromRGBO(112, 112, 112, 1);
+  static const OPERATION = Color.fromRGBO(250, 158, 13, 1);
+
   final String text;
   final bool big;
+  final Color color;
 
   Button({
     @required this.text,
     this.big = false,
+    this.color = DEFAULT,
+  });
+
+  Button.big({
+    @required this.text,
+    this.big = true,
+    this.color = DEFAULT,
+  });
+  Button.operation({
+    @required this.text,
+    this.big = false,
+    this.color = OPERATION,
   });
 
   @override
@@ -15,7 +32,15 @@ class Button extends StatelessWidget {
     return Expanded(
       flex: big ? 2 : 1,
       child: RaisedButton(
-        child: Text(text),
+        color: this.color,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 32,
+            fontWeight: FontWeight.w200,
+          ),
+        ),
         onPressed: () {},
       ),
     );
