2d5dddf87ca6b43decc7caaf035efb3a9a082c16
juliocezar96 <juliocr.jc38@gmail.com>
2021-03-15 19:22:58 -0300

    [calculator] - starting keyboard

---
+++ lib/components/keyboard.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Keyboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 500,
+      child: Column(
+        children: <Widget>[
+          RaisedButton(
+            child: Text('9'),
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
