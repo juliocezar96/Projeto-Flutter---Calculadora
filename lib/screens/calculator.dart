4e001fd6eee4116d56673f0ceee0f3ab3b0205bf
juliocezar96 <juliocr.jc38@gmail.com>
2021-03-15 19:14:49 -0300

    Projeto Calculadora

---
+++ lib/screens/calculator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import '../components/display.dart';
+
+class Calculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Column(
+      children: <Widget>[
+        Display('123'),
+        Text('Keyboard'),
+      ],
+    ));
+  }
+}
