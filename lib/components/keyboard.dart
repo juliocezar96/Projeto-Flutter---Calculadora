637650d1fb111c34376d48b501ac274c11fb2a95
juliocezar96 <juliocr.jc38@gmail.com>
2021-03-15 20:21:14 -0300

    [calculator] - auto clear

--- lib/components/keyboard.dart
+++ lib/components/keyboard.dart
@@ -40,7 +40,7 @@ class Keyboard extends StatelessWidget {
           ]),
           ButtonRow([
             Button.big(text: '0', cb: cb),
-            Button(text: ',', cb: cb),
+            Button(text: '.', cb: cb),
             Button.operation(text: '=', cb: cb),
           ]),
         ],
